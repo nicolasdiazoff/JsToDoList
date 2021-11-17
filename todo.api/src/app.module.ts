579c3b8c6d3930298a5dc9b7452bd68b8bbc4e25
Nicolas Diaz <nicolasdiazoff@gmail.com>
2021-11-17 16:55:53 -0500

    update: remove mysql integration

--- todo.api/src/app.module.ts
+++ todo.api/src/app.module.ts
@@ -1,24 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from './task/task.entity';
 import { TasksModule } from './task/task.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'todo_mysql',
-      port: 3306,
-      username: 'root',
-      password: '381136',
-      database: 'todo',
-      entities: [Task],
-      synchronize: true,
-    }),
-    TasksModule,
-  ],
+  imports: [TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
